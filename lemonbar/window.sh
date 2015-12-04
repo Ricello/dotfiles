d5563bed22dece13b503bcfd32af85fe05ef3aae
Sorebit <cmbk90@gmail.com>
2015-12-04 14:32:18 +0100

    Już jest ładniej

--- lemonbar/window.sh
+++ lemonbar/window.sh
@@ -8,7 +8,7 @@ while read window; do
 	if [ -z "$window" ]; then
 		window="No active window."
 	fi
-	echo "%{c}     $window  %{l}  \uf0c9"
+	echo "%{c}      $window  %{l}   \uf0c9"
 done |
 
 lemonbar -g 180x28+0+0 -b -d -B \#FF$background -F \#FF$foreground -f "Inconsolata:size=9" -f "FontAwesome:size=10"| bash
